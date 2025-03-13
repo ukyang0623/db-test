9485738b7ab7c98d9680f35a41b9cc567771ffba
ukyang <ukyang0623@gmail.com>
2025-03-14 01:28:42 +0800

    feat(main): create springboot module

---
+++ src/main/java/cn/com/atlas/test/db/DbTestApplication.java
@@ -0,0 +1,11 @@
+package cn.com.atlas.test.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DbTestApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/com/atlas/test/db/DbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.atlas.test.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
